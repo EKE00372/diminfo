a599c02d72616b47c59228559a0f5d7d303369e2
EK <eke00372@gmail.com>
2018-08-03 15:56:22 +0800

    update locale

---
+++ Locale/zhTW.lua
@@ -0,0 +1,48 @@
+﻿if GetLocale() ~= "zhTW" then return end
+
+infoL = {
+	["AutoSell junk"] = "自動賣垃圾：",
+	["Trash sold, earned "] = "垃圾售出：",
+	
+	["AutoRepair"] = "自動修理：",
+	["Repair cost covered by G-Bank"] = "修理花費公款：",
+	["Repair cost"] = "修理花費：",
+	["Go farm, newbie"] = "你真窮。",
+	["none"] = "無裝備",	
+	
+	["No Guild"] = "沒人要",
+	["Sorting"] = "排序",
+	["Sorting by:"] = "排序方式：",
+	
+	["Hidden"] = "隱藏",
+	["Shift"] = "Shift展開",
+	
+	["Default UI Memory Usage:"] = "内建插件資源耗用：",
+	["Total Memory Usage:"] = "總資源耗用：",
+	["Garbage collected"] = "釋放記憶體：",
+	["AutoCollect"] = "自動整理記憶體暫存：",
+
+	["No Talents"] = "無專精",
+
+	["My Position"] = "我的位置：",
+	
+	["System"] = "系統",
+	["Home"] = "本地",
+	["Latency"] = "延遲：",
+	["CPU Usage"] = "顯示CPU占用比例：",
+	["Reload UI(on)"] = "|cff777777dim|rinfo[|cff00ff00System|r]: 重載介面後顯示插件的CPU佔用。",
+	["Reload UI(off)"] = "|cff777777dim|rinfo[|cff00ff00System|r]: 重載介面後隱藏插件的CPU佔用。",
+	
+	--[[["New"] = "新",
+	["Mail"] = "有新信",
+	["No Mail"] = "無新信",	
+	["Earned:"] = "獲得：",
+	["Spent:"] = "花費：",
+	["Deficit:"] = "虧損：",
+	["Profit:"] = "盈利：",
+	["Session"] = "本次登入",
+	["Server"] = "伺服器",
+	["Character"] = "當前角色",
+	["Current"] = "目前",
+	["Daily"] = "今天",]]--
+}
