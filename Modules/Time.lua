de32542f67d054013d433b5ebf7c6169b2e00013
EK <eke00372@gmail.com>
2018-08-17 08:12:40 +0800

    time: update instance reset time function

--- Modules/Time.lua
+++ Modules/Time.lua
@@ -17,95 +17,48 @@ if cfg.Time == true then
 	Text:SetPoint(unpack(cfg.TimePoint))
 	Stat:SetAllPoints(Text)
 
-	local int = 1
-	local Hr24, Hr, Min
-	local function Update(self, t)
-		local pendingCalendarInvites = C_Calendar.GetNumPendingInvites()
-		int = int - t
-		if int < 0 then
+	local function Update(self, elapsed)
+		self.timer = (self.timer or 0) + elapsed
+		if self.timer > 1 then
+			-- 有邀請時變色
+			local color = C_Calendar.GetNumPendingInvites() > 0 and "|cffFF0000" or ""
 			-- 本地時間
-			if GetCVar("timeMgrUseLocalTime") == "1" then
-				Hr24 = tonumber(date("%H"))
-				Hr = tonumber(date("%I"))
-				Min = date("%M")
-				-- 24小時制
-				if GetCVar("timeMgrUseMilitaryTime") == "1" then
-					if pendingCalendarInvites > 0 then
-					Text:SetText("|cffFF0000"..Hr24..":"..Min)
-				else
-					Text:SetText(Hr24..":"..Min)
-				end
+			local hour, minute
+			if GetCVarBool("timeMgrUseLocalTime") then
+				hour, minute = tonumber(date("%H")), tonumber(date("%M"))
 			else
-				if Hr24>=12 then
-					if pendingCalendarInvites > 0 then
-						Text:SetText(cfg.ColorClass and "|cffFF0000"..Hr..":"..Min..init.Colored.."pm|r" or "|cffFF0000"..Hr..":"..Min.."|cffffffffpm|r")
-					else
-						Text:SetText(cfg.ColorClass and Hr..":"..Min..init.Colored.."pm|r" or Hr..":"..Min.."|cffffffffpm|r")
-					end
-				else
-					if pendingCalendarInvites > 0 then
-						Text:SetText(cfg.ColorClass and "|cffFF0000"..Hr..":"..Min..init.Colored.."am|r" or "|cffFF0000"..Hr..":"..Min.."|cffffffffam|r")
-					else
-						Text:SetText(cfg.ColorClass and Hr..":"..Min..init.Colored.."am|r" or Hr..":"..Min.."|cffffffffam|r")
-					end
-				end
+				hour, minute = GetGameTime()
 			end
-		else
-			local Hr, Min = GetGameTime()
-			if Min<10 then Min = "0"..Min end
-			if GetCVar("timeMgrUseMilitaryTime") == "1" then
-				if pendingCalendarInvites > 0 then			
-					Text:SetText("|cffFF0000"..Hr..":"..Min.."|cffffffff|r")
-				else
-					Text:SetText(Hr..":"..Min.."|cffffffff|r")
-				end
+			-- 24小時制
+			if GetCVarBool("timeMgrUseMilitaryTime") then
+				Text:SetText(format(color..TIMEMANAGER_TICKER_24HOUR, hour, minute))
 			else
-				if Hr>=12 then
-					if Hr>12 then Hr = Hr-12 end
-					if pendingCalendarInvites > 0 then
-						Text:SetText(cfg.ColorClass and "|cffFF0000"..Hr..":"..Min..init.Colored.."pm|r" or "|cffFF0000"..Hr..":"..Min.."|cffffffffpm|r")
-					else
-						Text:SetText(cfg.ColorClass and Hr..":"..Min..init.Colored.."pm|r" or Hr..":"..Min.."|cffffffffpm|r")
-					end
-				else
-					if Hr == 0 then Hr = 12 end
-					if pendingCalendarInvites > 0 then
-						Text:SetText(cfg.ColorClass and "|cffFF0000"..Hr..":"..Min..init.Colored.."am|r" or "|cffFF0000"..Hr..":"..Min.."|cffffffffam|r")
-					else
-						Text:SetText(cfg.ColorClass and Hr..":"..Min..init.Colored.."am|r" or Hr..":"..Min.."|cffffffffam|r")
-					end
-				end
+				Text:SetText(format(color..TIMEMANAGER_TICKER_12HOUR..init.Colored..(hour < 12 and "AM" or "PM"), hour, minute))
 			end
-		end		
-		int = 1
+	
+			self.timer = 0
 		end
-		self:SetAllPoints(Text)
 	end
+
 	
-	local months = {
-		MONTH_JANUARY,
-		MONTH_FEBRUARY,
-		ONTH_MARCH,
-		MONTH_APRIL,
-		MONTH_MAY,
-		MONTH_JUNE,
-		MONTH_JULY,
-		MONTH_AUGUST,
-		MONTH_SEPTEMBER,
-		MONTH_OCTOBER,
-		MONTH_NOVEMBER,
-		MONTH_DECEMBER,
-	}
-	
-	local function zsub(s,...) local t={...} for i=1,#t,2 do s=gsub(s,t[i],t[i+1]) end return s end
+	local function zsub(s,...)
+		local t={...}
+			for i=1,#t,2 do
+				s=gsub(s,t[i],t[i+1])
+			end
+		return s
+	end
 
 	Stat:SetScript("OnEnter", function(self)
+	
 		OnLoad = function(self) RequestRaidInfo() end
+		
 		local today = C_Calendar.GetDate()
 		local w, m, d, y = today.weekday, today.month, today.monthDay, today.year
+		
 		GameTooltip:SetOwner(self, "ANCHOR_BOTTOM", 0, -10)
 		GameTooltip:ClearLines()
-		--GameTooltip:AddLine(format(FULLDATE, CALENDAR_WEEKDAY_NAMES[w], months[m], d, y), 0, .6, 1)
+
 		GameTooltip:AddLine(format(FULLDATE, CALENDAR_WEEKDAY_NAMES[w], CALENDAR_FULLDATE_MONTH_NAMES[m], d, y), 0,.6,1)
 		GameTooltip:AddLine(" ")
 		GameTooltip:AddDoubleLine(TIMEMANAGER_TOOLTIP_LOCALTIME, zsub(GameTime_GetLocalTime(true), "%s*AM", "am", "%s*PM", "pm"), .6, .8, 1, 1, 1, 1)
@@ -134,28 +87,76 @@ if cfg.Time == true then
 		--Hr = tonumber(date("%I"))
 		--Min = date("%M")]]--
 		
-		local oneraid
-		for i = 1, GetNumSavedInstances() do
-			local name, _, reset, difficulty, locked, extended, _, isRaid, _, difficultyName  = GetSavedInstanceInfo(i)
-			--if isRaid and (locked or extended) then
-			if (locked or extended) then
-				if not oneraid then
-					GameTooltip:AddLine(" ")
-					GameTooltip:AddLine(LOCK_EXPIRE, .6, .8, 1)
-					oneraid = true
-				end
-			GameTooltip:AddDoubleLine(name, SecondsToTime(reset, true, nil, 3), r,g,b, 1,1,1)
-			GameTooltip:AddLine(difficultyName, 1,1,1)
+		local title
+		local function addTitle(text)
+			if not title then
+				GameTooltip:AddLine(" ")
+				GameTooltip:AddLine(text, .6, .8, 1)
+				title = true
 			end
 		end
+
 		-- world boss/世界首領
 		for i = 1, GetNumSavedWorldBosses() do
 			local name, id, reset = GetSavedWorldBossInfo(i)
 			if not (id == 11 or id == 12 or id == 13) then
-				AddTitle(RAID_INFO_WORLD_BOSS)
-				GameTooltip:AddDoubleLine(name, SecondsToTime(reset, true, nil, 3), 1,1,1, 1,1,1)
+				addTitle(RAID_INFO_WORLD_BOSS)
+				GameTooltip:AddDoubleLine(name, SecondsToTime(reset, true, nil, 3), 1, .82, 0, 1,1,1)
 			end
 		end
+		
+		-- 5H
+		title = false
+		for i = 1, GetNumSavedInstances() do
+			local name, _, reset, difficulty, locked, extended = GetSavedInstanceInfo(i)
+
+			if difficulty == 2 and (locked or extended) then
+				addTitle(PLAYER_DIFFICULTY2)
+				local r,g,b
+				if extended then
+					r,g,b = .3, 1, .3
+				else
+					r,g,b = 1,1,1
+				end
+			GameTooltip:AddDoubleLine(name, SecondsToTime(reset, true, nil, 3), 1, .82, 0, r,g,b)
+			end
+		end
+		
+		-- 5M
+		title = false
+		for i = 1, GetNumSavedInstances() do
+			local name, _, reset, difficulty, locked, extended = GetSavedInstanceInfo(i)
+
+			if difficulty == 23 and (locked or extended) then
+				addTitle(PLAYER_DIFFICULTY6)
+				local r,g,b
+				if extended then
+					r,g,b = .3, 1, .3
+				else
+					r,g,b = 1,1,1
+				end
+			GameTooltip:AddDoubleLine(name, SecondsToTime(reset, true, nil, 3), 1, .82, 0, r,g,b)
+			end
+		end
+
+		-- RAID
+		title = false
+		for i = 1, GetNumSavedInstances() do
+			local name, _, reset, _, locked, extended, _, isRaid, _, difficultyName  = GetSavedInstanceInfo(i)
+
+			if isRaid and (locked or extended) then
+				addTitle(GUILD_CHALLENGE_TYPE2)
+				local r,g,b
+				if extended then
+					r,g,b = .3, 1, .3
+				else
+					r,g,b = 1,1,1
+				end
+			GameTooltip:AddDoubleLine(difficultyName.." - "..name, SecondsToTime(reset, true, nil, 3), 1, .82, 0, r,g,b)
+			end
+		end
+		
+
 		
 		GameTooltip:Show()
 	end)
